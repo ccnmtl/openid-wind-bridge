0148aa15f479f75d5f042f957da1feb8ba8ded77
sky <sky@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-08-04 22:53:16 +0000

    fake uni meets xrds requirements in .htaccess
    git-svn-id: http://svn.ccnmtl.columbia.edu/openid-wind-bridge/trunk@15831 1f418930-7ff8-0310-b8ad-c653122473bc

--- fake.php
+++ fake.php
@@ -20,9 +20,8 @@ if (function_exists('getOpenIDStore')) {
 
     init();
 
-    
     /*******FAKE ***********/
-    $_SESSION['wind_user'] = 'XXFAKEUSERXX';
+    $_SESSION['wind_user'] = 'XX-FAKEUNI-XX1';
     $_SESSION['wind_groups'] = array('FAKE_CLASS.st.FAKEFAKEFAKE');
     $_POST = array("openid_ns"=>"http://specs.openid.net/auth/2.0",
 		   "openid_ns_sreg"=>"http://openid.net/extensions/sreg/1.1",
